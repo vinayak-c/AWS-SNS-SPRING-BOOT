198e313a035b331b296962934afae1ca74f1f69d
Vinayak <vinayakchaurasia99@gmail.com>
2022-08-14 00:13:44 +0530

    Added AWS SNS config

---
+++ src/main/java/com/vinayak/aws/sns/config/AWSSNSConfig.java
@@ -0,0 +1,25 @@
+package com.vinayak.aws.sns.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AWSSNSConfig {
+
+    public static final String SECRET_KEY = "Put secret key";
+    public static final String ACCESS_KEY = "Put access key";
+
+    @Primary
+    @Bean
+    public AmazonSNSClient getSnsClient() {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+                .build();
+    }
+}
